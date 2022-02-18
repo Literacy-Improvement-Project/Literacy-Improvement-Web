158bb69d85e2b511726444f5db44b13293ab071f
김시우 <tldn12gh@gmail.com>
2022-02-19 00:00:09 +0900

    Add Literacy-Improvement-Web Back-End

---
+++ backend/KotudyPrj/src/main/java/com/kotudyprj/dao/IVocabularyNoteDao.java
@@ -0,0 +1,20 @@
+package com.kotudyprj.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.kotudyprj.dto.VocabularyNoteDto;
+
+
+@Mapper
+public interface IVocabularyNoteDao {
+	
+	public List<String> showWord(@Param(value = "_userId") String userId);
+	public int checkWord(@Param(value = "_userId") String userId ,@Param(value = "_word") String q);
+	public void addWord(@Param(value = "_userId") String userId ,@Param(value = "_word") String q, @Param(value = "_mean") String p);
+	public void deleteWord(@Param(value = "_userId") String userId, @Param(value = "_word") String word);
+	public List<VocabularyNoteDto> getVocabularynote();
+
+}
