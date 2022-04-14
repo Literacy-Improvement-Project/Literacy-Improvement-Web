2adb9653bb72e8a4f16593f69d25d54f47383b13
Cheolsoon <mn5724@naver.com>
2022-04-14 20:52:09 +0900

    merging

--- frontend/nextjs-literacy-web/components/atom/buttons/button.js
+++ frontend/nextjs-literacy-web/components/atom/buttons/button.js
@@ -3,6 +3,6 @@ import styles from './Button.module.css';
 export default function Button(props) {
     return (
         <button className={`${styles.btn3} ${styles.custombtn}`}
-            onClick={props.click}><span>Read More</span></button>
+            onClick={props.onClick}><span>{props.label}</span></button>
     )
 }
